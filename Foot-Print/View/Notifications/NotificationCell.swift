f8a8defb651a38d7ff04acee848fca4c49a301fd
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-14 18:46:37 +0530

    updated notofocation view

--- Foot-Print/View/Notifications/NotificationCell.swift
+++ Foot-Print/View/Notifications/NotificationCell.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct NotificationCell: View {
+    
+    @State var width = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Rectangle()
+                .frame(width: width, height: 1, alignment: .center)
+                .foregroundColor(.gray)
+                .opacity(0.3)
+            
+            HStack(alignment: .top) {
+                
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .foregroundColor(.blue)
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                
+                VStack(alignment: .leading, spacing: 5, content: {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 36, height: 36)
+                        .cornerRadius(18)
+                    
+                    Text("Solo ")
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                    
+                    +
+                    
+                    Text("followed you")
+                        .foregroundColor(.black)
+                })
+                
+                Spacer(minLength: 0)
+            }
+            .padding(.leading, 30)
+        }
     }
 }
 
