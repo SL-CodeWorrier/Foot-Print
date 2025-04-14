b0bcfd6f4b38323c4157cd140fe2c8bf58fd36a6
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-14 18:45:55 +0530

    Created notification view

---
+++ Foot-Print/View/Notifications/NotificationCell.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationCell.swift
+//  Foot-Print
+//
+//  Created by Chathura Aththanayaka on 2025-04-14.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationCell()
+}
