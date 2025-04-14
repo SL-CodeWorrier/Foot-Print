f4fd0a647522402336a8dec4e09815addb667c96
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-14 08:48:28 +0530

    created search view

---
+++ Foot-Print/View/Search/SearchCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCell.swift
+//  Foot-Print
+//
+//  Created by Chathura Aththanayaka on 2025-04-14.
+//
+
+import SwiftUI
+
+struct SearchCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchCell()
+}
