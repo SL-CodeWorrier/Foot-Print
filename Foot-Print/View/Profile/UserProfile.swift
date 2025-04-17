b4fe68e3782bbae20155827b504267ee49b8c4e1
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-17 10:51:17 +0530

    profile bio created

--- Foot-Print/View/Profile/UserProfile.swift
+++ Foot-Print/View/Profile/UserProfile.swift
@@ -30,7 +30,7 @@ struct UserProfile: View {
                             Image("banner")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 0 : 180 + minY, alignment: .center)
+                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 180 + minY : 180, alignment: .center)
                                 .cornerRadius(0)
                             
                             BlurView()
@@ -56,6 +56,65 @@ struct UserProfile: View {
                 .frame(height: 180)
                 .zIndex(1)
                 
+                VStack {
+                    HStack {
+                        
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white)
+                            .offset(y: offset < 0 ? getOffset() - 20 : -20)
+                            .scaleEffect(getScale())
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Edit Profile")
+                                .foregroundColor(.blue)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(Capsule()
+                                .stroke(Color.blue, lineWidth: 1.5))
+                        })
+                    }
+                    .padding(.top, -25)
+                    .padding(.bottom, -10)
+                    
+                    VStack(alignment: .leading, spacing: 8, content: {
+                        Text("Solo")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        
+                        Text("@Solo_hiker")
+                            .foregroundColor(.gray)
+                        
+                        Text("I want the best love story in the world to be written in the name of me and nature.")
+                        
+                        HStack(spacing: 5, content: {
+                            Text("13")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                            
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                            
+                            Text("680")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                                .padding(.leading, 10)
+                            
+                            Text("Following")
+                                .foregroundColor(.gray)
+                        })
+                    })
+                }
+                
             }
         }
     }
@@ -69,6 +128,17 @@ struct UserProfile: View {
         let progress = 20 / titleOffset
         let offset = 60 * (progress > 0 && progress <= 1 ? progress : 1)
         return offset
+    }
+    
+    func getOffset() -> CGFloat {
+        let progress = (-offset/80) * 80
+        return progress <= 20 ? progress : 20
+    }
+    
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1)
+        return scale  < 1 ? scale : 1
     }
 }
 
