e694246bc931771fce28adee20adc02d2a369a33
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-17 08:14:00 +0530

    userprofile again updated

--- Foot-Print/View/Profile/BlurView.swift
+++ Foot-Print/View/Profile/BlurView.swift
@@ -5,4 +5,17 @@
 //  Created by Chathura Aththanayaka on 2025-04-17.
 //
 
-import Foundation
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+}
