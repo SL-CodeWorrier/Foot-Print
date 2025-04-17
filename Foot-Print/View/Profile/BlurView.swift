f9fa0be5e6bbdfd30ebc24e95a0a7eea59f7ad0e
SL-CodeWorrier <chathuathnayaka@gmail.com>
2025-04-17 08:13:10 +0530

    user profile updated

---
+++ Foot-Print/View/Profile/BlurView.swift
@@ -0,0 +1,8 @@
+//
+//  BlurView.swift
+//  Foot-Print
+//
+//  Created by Chathura Aththanayaka on 2025-04-17.
+//
+
+import Foundation
---
+++ Foot-Print/View/Profile/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  Foot-Print
+//
+//  Created by Chathura Aththanayaka on 2025-04-17.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
